69fb01f1cfe0bc9ada1f89ea2d64d74b08fdeb9f
Tobias <57042329+tobilinz@users.noreply.github.com>
2023-12-19 06:58:06 +0000

    Updated counter code.

--- src/script.js
+++ src/script.js
@@ -7,12 +7,13 @@ function countDown() {
     const seconds = document.getElementById("seconds");
 
     const date = Date.parse("2024-02-09T19:00:00.000+01:00");
-    const now = Date.now();
-
-    let diff = date - now;
 
     function updateCounter() {
-        const daysLeft = diff / (1000 * 60 * 60 * 24);
+        const timeLeft = date - Date.now();
+
+        if (timeLeft <= 0) return false;
+
+        const daysLeft = timeLeft / (1000 * 60 * 60 * 24);
         days.textContent = Math.floor(daysLeft).toString();
         const daysRest = daysLeft % 1;
         const hoursLeft = daysRest * 24;
@@ -23,22 +24,19 @@ function countDown() {
         const minutesRest = minutesLeft % 1;
         const secondsLeft = minutesRest * 60;
         seconds.textContent = Math.floor(secondsLeft).toString();
+
+        return true;
     }
 
-    updateCounter()
+    if (!updateCounter()) return;
 
-    const countdownTimer = setInterval(function () {
-        diff -= 1000;
+    const countdownTimer = setInterval(() => { 
+        if (updateCounter()) return;
 
-        if (diff <= 0) {
-            clearInterval(countdownTimer);
-            days.textContent = "0";
-            hours.textContent = "0";
-            minutes.textContent = "0";
-            seconds.textContent = "0";
-            return;
-        }
-
-        updateCounter()
+        clearInterval(countdownTimer);
+        days.textContent = "0";
+        hours.textContent = "0";
+        minutes.textContent = "0";
+        seconds.textContent = "0";
     }, 1000);
 }
